b10dfb818d80d4e692f6246d437f509ac0aa56a7
vanderpatrick <patrick123321123@icloud.com>
2022-07-19 12:37:00 +0000

    a

---
+++ scripts/tests/buttons.test.js
@@ -0,0 +1,20 @@
+// const buttonClick = require("../buttons");
+
+
+// beforeEach(() =>{
+//     let fs = require("fs");
+//     let fileContents = fs.readFileSync("index.html","utf-8");
+//     document.open();
+//     document.write(fileContents);
+//     document.close();
+// });
+
+// describe("DOM test", ()=>{
+//     test("expects p content to change", ()=>{
+//         buttonClick();
+//         expect(document.getElementById("par").innerHTML).toBe("You Clicked")
+//     });
+//     test("h1 should exist", ()=>{
+//         expect(document.getElementsByTagName('h1').length).toBe(1);
+//     });
+// });
